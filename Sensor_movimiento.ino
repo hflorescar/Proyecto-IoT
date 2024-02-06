80cc55e60d9475e002aa18257e865045bd4264fd
Aldunate8 <153854945+Aldunate8@users.noreply.github.com>
2024-02-06 19:40:06 +0100

    Add files via upload

---
+++ Sensor_movimiento.ino
@@ -0,0 +1,31 @@
+const int sensorPin = 36;
+ int sensorValue = 0;
+ int presencia = 0;
+ const int led = 12;
+
+ static int value = 1000;
+
+void setup() {
+ Serial.begin(9600);
+ pinMode(sensorPin,INPUT);
+ pinMode(led, OUTPUT);
+  }
+
+void loop() {
+sensorValue = analogRead(sensorPin);
+if (sensorValue <= 800 || sensorValue >= 3200){Serial.print("Persona detectada");
+presencia = 1;
+} 
+else{
+  presencia = 0;
+  }
+  Serial.print(sensorValue);
+if(presencia==1){
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led,LOW);
+}
+ else{
+  digitalWrite(led,LOW);
+}
+}
